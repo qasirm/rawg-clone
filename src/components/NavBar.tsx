85b3240616c66dfa63846b3e937cc639428f4d8b
qasirm <harveymo@usc.edu>
2023-09-09 19:36:49 -0700

    Dark Mode Switch

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,14 +1,13 @@
 import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/game-hub.webp";
-import React from "react";
+import ColorModeSwitch from "./ColorModeSwitch";
 
 const NavBar = () => {
   return (
-    <div>
-      <HStack>
-        <Image src={logo} boxSize="60px" />
-      </HStack>
-    </div>
+    <HStack justifyContent="space-between" padding="10px">
+      <Image src={logo} boxSize="60px" />
+      <ColorModeSwitch />
+    </HStack>
   );
 };
 
